21972495168b3d8f3311e767ac8854c2d4e0a9e1
Mairon Gabriel Mariñez Brioso <mmarinez@hidroelectrica.gov.do>
2020-09-10 11:16:36 -0400

    created navar

--- projectP/src/app/app.module.ts
+++ projectP/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavarComponent } from './component/navar/navar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavarComponent
   ],
   imports: [
     BrowserModule,
