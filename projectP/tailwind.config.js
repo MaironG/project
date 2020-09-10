dc276455eba9e3d4c555afbaf886f430ce34bf4d
Mairon Gabriel Mariñez Brioso <mmarinez@hidroelectrica.gov.do>
2020-09-10 11:15:58 -0400

    instalacion de teilwind

---
+++ projectP/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      'letra': ['Fredoka One', 'cursive'],
+      'sans': ['Helvetica', 'Arial', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
